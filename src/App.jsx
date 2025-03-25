3b5302a16faafeba632493c1750278c66460973c
yash samar <yashsamar7@gmail.com>
2025-03-25 12:04:57 +0530

    Updated the website with new changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./index.css";
 import { LoadingScreen } from "./components/LoadingScreen";
 import { Navbar } from "./components/Navbar";
 import { useState } from "react";
-import {MobileMenu} from "./components/MobileMenu"
+// import {MobileMenu} from "./components/MobileMenu"
 import { Home } from "./components/sections/Home";
 import {About} from "./components/sections/About"
 import { Projects } from "./components/sections/Projects";
@@ -13,14 +13,14 @@ function App() {
   const [menuOpen,setMenuOpen] = useState(false);
   return (
     <>
-      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
       <div
         className={`min-h-screen transition-opacity duration-700 ${
           isLoaded ? "opacity-100" : "opacity-0"
         } bg-black text-gray-100 `}
       >
          <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-     <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+     {/* <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/> */}
      <Home />
      <About />
      <Projects/>
